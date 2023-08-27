d90ad0bce1b9d0888cf9e9acd2b47273a95c6348
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-08-27 11:33:11 -0400

    adding add to cart button

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,18 +1,65 @@
-
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Product({ product }) {
-    return (
-        <div className="product">
-            <p className="label"><span className="bold centered-type">{product.product_type}</span></p>
-            <img src={product.url} alt={product.product_name} />
-            <p className="label"><span className="bold">Price: </span>${product.price}</p>
-            <p className="label"><span className="bold">Favorit:</span> {product.is_favorite ? (
-                <span>⭐</span>
-            ) : (
-                <span>✩</span>
-            )}</p>
-            <Link to={`/products/${product.id}`}><span className="bold"></span> {product.product_name}</Link>
+  const [quantity, setQuantity] = useState(0);
+
+  const handleQuantityChange = (newQuantity) => {
+    if (newQuantity >= 0) {
+      setQuantity(newQuantity);
+    }
+  };
+
+  const addToCart = () => {
+    setQuantity(quantity + 1);
+    console.log(`Added ${quantity} ${product.product_name} to cart`);
+  };
+
+  return (
+    <div className="product">
+      <p className="label">
+        <span className="bold centered-type">
+          {product.product_type.toUpperCase()}
+        </span>
+      </p>
+      <img src={product.url} alt={product.product_name} />
+      <p className="label">
+        <span className="bold">Price: </span>${product.price}
+      </p>
+      <p className="label">
+        <span className="bold">Favorite:</span>{" "}
+        {product.is_favorite ? <span>⭐</span> : <span>✩</span>}
+      </p>
+      <Link to={`/products/${product.id}`}>
+        <span className="bold">{product.product_name}</span>
+      </Link>
+      <div className="product-details">
+        <div className="quantity-box">
+          <button
+            className="quantity-button"
+            onClick={() => handleQuantityChange(quantity - 1)}
+          >
+            -
+          </button>
+          <input
+            className="quantity-input"
+            type="number"
+            value={quantity}
+            onChange={(e) => handleQuantityChange(parseInt(e.target.value))}
+            min="1"
+          />
+
+          <button
+            className="quantity-button"
+            onClick={() => handleQuantityChange(quantity + 1)}
+          >
+            +
+          </button>
         </div>
-    )
+        <button onClick={addToCart} className="add-to-cart-button">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 }
